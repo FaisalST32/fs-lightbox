2d70c81cad5b495bfd4660a6870da3d8439a82be
FRashid <frashid@liventus.in>
2019-07-05 17:15:17 +0530

    Initial Commit

---
+++ fs-lightbox.js
@@ -0,0 +1,23 @@
+﻿// A bare-minimum Lightbox plugin developed at PQube Business Solutions
+// Usage: Add an onclick listenter to the img tag that you want to display in a lightbox
+// eg: <img src="/images/image.jpb" onclick="lightbox(this)" />
+// Do not forget to include the css file also
+
+
+function lightbox(_el) {
+    var overlay = document.createElement('div');
+    overlay.classList.add('lightbox-overlay');
+    overlay.addEventListener('click', hideLightbox);
+    var image = document.createElement('img');
+    image.src = _el.src;
+    image.classList.add('lightbox-image');
+    document.querySelector('body').appendChild(overlay);
+    document.querySelector('body').appendChild(image);
+}
+
+function hideLightbox() {
+    var overlay = document.querySelector('.lightbox-overlay');
+    var image = document.querySelector('.lightbox-image');
+    document.querySelector('body').removeChild(overlay);
+    document.querySelector('body').removeChild(image);
+}
